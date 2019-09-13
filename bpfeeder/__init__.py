870c2b8cd78c421eed47f30fb85eccd4067df7b0
jinho3838 <jinho3838@naver.com>
2019-09-13 23:35:18 +0900

    symbol depreciated

--- bpfeeder/__init__.py
+++ bpfeeder/__init__.py
@@ -1,16 +1,11 @@
 from .feeder import Feeder
 from .yahoo import yahoo
 from .naver import naver
-from .symbol import *
 
 exchanges = [
     'yahoo',
     'naver'
 ]
 
-symbols = [
-    'BloombergSymbol'
-]
-
-__all__ = exchanges + symbols
+__all__ = exchanges
 
